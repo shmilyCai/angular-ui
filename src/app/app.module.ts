c2c4b113a7d4943aeb29f970680fb0ab00739a68
caicai <shmily.cai@gmail.com>
2019-01-22 11:15:06 +0800

    demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule }  from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 
 import { AppComponent } from './app.component';
